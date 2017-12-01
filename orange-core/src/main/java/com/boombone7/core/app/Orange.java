325bd39678c202e2d4999fca7b0e1289e035e6b8
Ting <ticccccc@163.com>
2017-12-01 13:21:29 +0800

    添加核心模块
    (cherry picked from commit 5fdb1b2)

---
+++ orange-core/src/main/java/com/boombone7/core/app/Orange.java
@@ -0,0 +1,36 @@
+package com.boombone7.core.app;
+
+import android.content.Context;
+import android.os.Handler;
+
+import com.boombone7.core.I;
+
+import java.util.HashMap;
+
+/**
+ * @author Ting
+ * @date 2017/11/9
+ */
+
+public final class Orange {
+    public static Configurator init(Context context) {
+        getConfigs().put(I.Configkey.APPLICATION_CONTEXT, context.getApplicationContext());
+        return Configurator.getInstance();
+    }
+
+    private static HashMap<String, Object> getConfigs() {
+        return Configurator.getInstance().getConfig();
+    }
+
+    public static <T> T getConfiguration(String key) {
+        return Configurator.getInstance().getConfiguration(key);
+    }
+
+    public static Context getApplicationContext() {
+        return getConfiguration(I.Configkey.APPLICATION_CONTEXT);
+    }
+
+    public static Handler getHandler() {
+        return getConfiguration(I.Configkey.HANDLER);
+    }
+}
