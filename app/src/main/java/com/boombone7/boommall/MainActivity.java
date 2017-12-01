93c066d8ebef85cbeeea238458fbde53e4bd28d2
Ting <ticccccc@163.com>
2017-12-01 15:49:24 +0800

    单Activity界面架构设完成

--- app/src/main/java/com/boombone7/boommall/MainActivity.java
+++ app/src/main/java/com/boombone7/boommall/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Toast;
 
 
+import com.boombone7.core.activities.ProxyActivity;
+import com.boombone7.core.delegates.OrangeDelegate;
 import com.boombone7.core.net.rx.RxRestClient;
 
 import io.reactivex.Observer;
@@ -17,41 +19,11 @@ import io.reactivex.schedulers.Schedulers;
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ProxyActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onClick(View view) {
-        RxRestClient.builder()
-                .url("index")
-                .build()
-                .get()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<String>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(@NonNull String s) {
-                        Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
+    public OrangeDelegate setRootDelegate() {
+        return new ExampleDelegate();
     }
 }
