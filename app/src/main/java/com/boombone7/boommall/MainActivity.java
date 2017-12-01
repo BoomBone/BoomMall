693c24d66f0646c397dc01a836bea955f069a88d
Ting <ticccccc@163.com>
2017-12-01 16:53:19 +0800

    完成闪屏页面

--- app/src/main/java/com/boombone7/boommall/MainActivity.java
+++ app/src/main/java/com/boombone7/boommall/MainActivity.java
@@ -1,20 +1,10 @@
 package com.boombone7.boommall;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
 
 import com.boombone7.core.activities.ProxyActivity;
 import com.boombone7.core.delegates.OrangeDelegate;
-import com.boombone7.core.net.rx.RxRestClient;
+import com.boombone7.orange.ec.launcher.LauncherDelegate;
 
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * @author Administrator
@@ -24,6 +14,6 @@ public class MainActivity extends ProxyActivity {
 
     @Override
     public OrangeDelegate setRootDelegate() {
-        return new ExampleDelegate();
+        return new LauncherDelegate();
     }
 }
