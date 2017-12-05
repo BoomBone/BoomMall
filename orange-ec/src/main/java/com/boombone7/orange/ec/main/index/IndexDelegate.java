5ba3623ae2901cec899093b8abc7550caca477d7
Ting <ticccccc@163.com>
2017-12-05 09:48:08 +0800

    首页下拉刷新实现
    (cherry picked from commit 7c60367)

--- orange-ec/src/main/java/com/boombone7/orange/ec/main/index/IndexDelegate.java
+++ orange-ec/src/main/java/com/boombone7/orange/ec/main/index/IndexDelegate.java
@@ -2,18 +2,45 @@ package com.boombone7.orange.ec.main.index;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.boombone7.core.delegates.bottom.BottomItemDelegate;
+import com.boombone7.core.ui.refresh.RefreshHandler;
 import com.boombone7.orange.ec.R;
+import com.boombone7.orange.ec.R2;
+import com.joanzapata.iconify.widget.IconTextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
- *
  * @author Ting
  * @date 2017/12/4
  */
 
 public class IndexDelegate extends BottomItemDelegate {
+    @BindView(R2.id.rv_index)
+    RecyclerView mRvIndex;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mSrlIndex;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconIndexScan;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mEtSearchView;
+    @BindView(R2.id.icon_index_message)
+    IconTextView mIconIndexMessage;
+    @BindView(R2.id.tb_index)
+    Toolbar mTbIndex;
+
+    RefreshHandler mRefreshHandler;
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_index;
@@ -21,6 +48,21 @@ public class IndexDelegate extends BottomItemDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+        mRefreshHandler = new RefreshHandler(mSrlIndex);
+    }
 
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+        initRefresh();
+    }
+
+    private void initRefresh() {
+        mSrlIndex.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_green_light
+        );
+        mSrlIndex.setProgressViewOffset(true,123,300);
     }
 }
