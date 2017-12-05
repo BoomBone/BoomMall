41ebfce479f01ee5fa4208dc8467332b087dc54b
BoomBone <ticccccc@163.com>
2017-12-05 18:22:29 +0800

    完成首页的基本布局
    (cherry picked from commit ef3d523)

--- orange-core/src/main/java/com/boombone7/core/delegates/OrangeDelegate.java
+++ orange-core/src/main/java/com/boombone7/core/delegates/OrangeDelegate.java
@@ -4,5 +4,9 @@ package com.boombone7.core.delegates;
  * Created by Ting on 2017/12/1.
  */
 
-public abstract class OrangeDelegate extends PermissionCheckerDelegate{
+public abstract class OrangeDelegate extends PermissionCheckerDelegate {
+
+    public <T extends OrangeDelegate> T getParentDelegate(){
+        return (T) getParentFragment();
+    }
 }
