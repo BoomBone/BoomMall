b8af975ca8528441afaaeb1028bafca62d7da603
Ting <ticccccc@163.com>
2017-12-22 09:47:36 +0800

    图片处理剪裁和动态权限封装2.2

--- orange-core/src/main/java/com/boombone7/core/delegates/PermissionCheckerDelegate.java
+++ orange-core/src/main/java/com/boombone7/core/delegates/PermissionCheckerDelegate.java
@@ -1,14 +1,21 @@
 package com.boombone7.core.delegates;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.Toast;
 
 import com.boombone7.core.ui.camera.OrangeCamera;
 
 import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
 import permissions.dispatcher.RuntimePermissions;
 
 /**
@@ -28,6 +35,41 @@ public abstract class PermissionCheckerDelegate extends BaseDelegate {
     public void startCameraWithCheck(){
         PermissionCheckerDelegatePermissionsDispatcher.startCameraWithPermissionCheck(this);
     }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void onCameraDenied() {
+        Toast.makeText(getContext(), "不允许拍照", Toast.LENGTH_LONG).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void onCameraNever() {
+        Toast.makeText(getContext(), "永久拒绝权限", Toast.LENGTH_LONG).show();
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    void onCameraRationale(PermissionRequest request) {
+        showRationaleDialog(request);
+    }
+
+    private void showRationaleDialog(final PermissionRequest request) {
+        new AlertDialog.Builder(getContext())
+                .setPositiveButton("同意使用", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("拒绝使用", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .setMessage("权限管理")
+                .show();
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
