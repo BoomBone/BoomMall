2cfdc6ce0bd3618fe0e6f16c0b69d01df2233d1a
Ting <ticccccc@163.com>
2017-12-28 15:18:49 +0800

    仿微信自动多图展示及删除控件1

---
+++ orange-core/src/main/java/com/boombone7/core/ui/widget/AutoPhotoLayout.java
@@ -0,0 +1,60 @@
+package com.boombone7.core.ui.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.boombone7.core.R;
+import com.boombone7.core.delegates.OrangeDelegate;
+import com.joanzapata.iconify.widget.IconTextView;
+
+import java.util.ArrayList;
+
+/**
+ * @author Ting
+ * @date 2017/12/28
+ */
+
+public class AutoPhotoLayout extends LinearLayoutCompat {
+
+    private int mCurrentNum = 0;
+    private final int mMaxNum;
+    private final int mMaxLineNum;
+    private IconTextView mIconAdd = null;
+    private LayoutParams mParams = null;
+    //要删除的图片ID
+    private int mDeleteId = 0;
+    private AppCompatImageView mTargetImageView = null;
+    private final int mImageMargin;
+    private OrangeDelegate mDelegate = null;
+    private ArrayList<View> mLineViews = null;
+    private AlertDialog mTargetDialog = null;
+    private static final String ICON_TEXT = "{fa-plus}";
+    private final float mIconSize;
+
+    private final ArrayList<ArrayList<View>> ALL_VIEWS = new ArrayList<>();
+    private final ArrayList<Integer> LINE_HEIGHTS = new ArrayList<>();
+
+
+    public AutoPhotoLayout(Context context) {
+        this(context, null);
+    }
+
+    public AutoPhotoLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AutoPhotoLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.camera_flow_layout);
+        mMaxNum = typedArray.getInt(R.styleable.camera_flow_layout_max_count, 1);
+        mMaxLineNum = typedArray.getInt(R.styleable.camera_flow_layout_line_count, 3);
+        mImageMargin = typedArray.getInt(R.styleable.camera_flow_layout_item_margin, 0);
+        mIconSize = typedArray.getDimension(R.styleable.camera_flow_layout_icon_size, 20);
+        typedArray.recycle();
+    }
+}
